671948152e6fe4584386ebfc7942624ca0df9ed7
Luc Ferrara <lucjferrara@gmail.com>
2024-11-13 19:45:10 -0500

    Made get current weekly report show the total times for each project/task, and overall total time

--- run.py
+++ run.py
@@ -4,8 +4,7 @@ import json
 from dotenv import load_dotenv
 import os
 import math 
-from util import get_now, get_last_sunday
-
+from util import seconds_to_timestring, seconds_to_timestring_hhmmss, get_now, get_last_sunday
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 ACCOUNT_ID = os.getenv("ACCOUNT_ID")
@@ -53,17 +52,31 @@ def get_current_weekly_report(workspace):
     response_json = response.json()
     print_formatted_json(response_json)
     total_time = response_json["totals"][0]["totalTime"]
-    hours = math.floor(total_time / 60 / 60)
-    minutes = math.floor((total_time / 60) % 60)
-    seconds = math.floor(total_time % 60)
-    time_string = str(hours) + " hours, " + str(minutes) + " minutes " + str(seconds) + " seconds."
+    time_string = seconds_to_timestring(total_time)
+    time_string_hhmmss = seconds_to_timestring_hhmmss(total_time)
     print("Total time: ", time_string)
+    print("Total time: (HH/MM/SS) = ", time_string_hhmmss)
+    time_for_each_project = {}
     for i in range(0, len(response_json["timeentries"])):
         entry = response_json["timeentries"][i]
         duration = entry["timeInterval"]["duration"]
         project = entry['projectName']
         task = entry['taskName']
-        print("Entry ", i, ") ", duration, "| ", project, " | ", task, " |")
+        
+        if project not in time_for_each_project.keys():
+            time_for_each_project[project] = {}
+        if task not in time_for_each_project[project].keys():
+            time_for_each_project[project][task] = duration
+        else:
+            time_for_each_project[project][task] = time_for_each_project[project][task] + duration
+
+    print("-------------------------------")
+    for project in time_for_each_project:
+        project_time = sum(time_for_each_project[project].values())
+        print(project, ": ", seconds_to_timestring(project_time))
+        for task, duration in time_for_each_project[project].items():
+            print("         ", task, ":", seconds_to_timestring_hhmmss(duration))
+        print("-------------------------------")
 
 workspaces = get_workspaces()
 print(workspaces)
